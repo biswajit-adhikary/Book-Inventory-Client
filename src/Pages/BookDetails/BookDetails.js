f478755930c102cd210eb8fac953ad0ea3f511ab
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-07 14:36:17 +0600

    Single page data loded sucessfully

--- src/Pages/BookDetails/BookDetails.js
+++ src/Pages/BookDetails/BookDetails.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import banner from "../../images/banner.jpg";
 
 const BookDetails = () => {
     const { bookId } = useParams();
+
+    const [book, setBook] = useState({});
+    useEffect(() => {
+        const url = `http://localhost:5000/book/${bookId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setBook(data));
+    }, [])
+
     return (
         <div className='book-details-area'>
-            <div className='banner-area text-center d-flex justify-content-center align-items-center' style={{ backgroundImage: `url(${banner})` }}>
+            <div className='banner-area text-center d-flex justify-content-center align-items-center' style={{ backgroundImage: `url(${book.image})` }}>
                 <Container>
                     <div className="banner-text">
-                        <h3 className='theme-text-primary text-white'>This is book details: {bookId}</h3>
+                        <h3 className='theme-text-primary text-white'>Book: {book.name}</h3>
                     </div>
                 </Container>
             </div >
